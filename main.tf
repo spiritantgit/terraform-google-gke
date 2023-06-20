1c588b1719e4b594a7011958507565b82421fa2c
thejovelove <thejovelove@gmail.com>
2023-06-20 13:28:38 +0000

    comm4

--- main.tf
+++ main.tf
@@ -2,6 +2,6 @@ module "gke_cluster" {
   source         = "github.com/spiritantgit/tf-google-gke-cluster.git"
   GOOGLE_REGION  = var.GOOGLE_REGION
   GOOGLE_PROJECT = var.GOOGLE_PROJECT
-  GKE_NUM_NODES  = var.GKE_NUM_NODES
+  GKE_NUM_NODES  = 2
 }
 
