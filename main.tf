30fca9db82bb9e77d26c683611de2e1e3e6b16c3
thejovelove <thejovelove@gmail.com>
2023-06-20 12:36:29 +0000

    com2

--- main.tf
+++ main.tf
@@ -2,5 +2,12 @@ module "gke_cluster" {
   source         = "github.com/spiritant/terraform-google-gke"
   GOOGLE_REGION  = var.GOOGLE_REGION
   GOOGLE_PROJECT = var.GOOGLE_PROJECT
-  GKE_NUM_NODES  = 2
+  GKE_NUM_NODES  = var.GKE_NUM_NODES
+}  
+
+resource "google_container_cluster" "demo1" {
+  name = "demo1"
+  location = var.GOOGLE_REGION
+
+  initial_node_count = 3
 }
---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "GOOGLE_REGION" {
+    type = string
+    default = "us-central1-c"
+    description = "GCP region name"
+}
+
+variable "GOOGLE_REGION" {
+    type = string
+    description = "GCP project name"
+}
+
+
