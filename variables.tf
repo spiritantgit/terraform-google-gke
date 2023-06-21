0e67f2721d3bc83d61fbdd1fe0ad14c27b265c55
thejovelove <thejovelove@gmail.com>
2023-06-21 10:23:25 +0000

    flux1

--- variables.tf
+++ variables.tf
@@ -9,4 +9,24 @@ variable "GOOGLE_PROJECT" {
   description = "GCP project name"
 }
 
+variable "GITHUB_OWNER" {
+  type        = string
+  description = "GitHUb owner repository to use"
+}
 
+variable "GITHUB_TOKEN" {
+  type        = string
+  description = "GitHUb personal access token"
+}
+
+variable "FLUX_GITHUB_REPO" {
+  type        = string
+  default = "flux-gitops"
+  description = "Flux GitOps repository"
+}
+
+variable "FLUX_GITHUB_TARGET_PATH" {
+  type        = string
+  default = "clusters"
+  description = "Flux manifest subdirectory"
+}
--- vars.tfvars
+++ vars.tfvars
@@ -1,3 +1,4 @@
 GOOGLE_PROJECT = "spiritant"
 GOOGLE_REGION  = "us-central1-c"
 GKE_NUM_NODES  = 2
+GITHUB_OWNER     = "spiritantgit"
